4a394bedf36c0219b75edac41d35bd658dd84035
Guilherme Felipe <glipecoelho2@gmail.com>
2023-08-30 18:23:40 -0300

    Second commit. Imported JNA library for the project using Maven and minor increments in Main application.

--- src/main/java/com/Application/Main.java
+++ src/main/java/com/Application/Main.java
@@ -1,7 +1,13 @@
 package com.Application;
 
-public class Main {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.awt.event.InputEvent;
+import static java.lang.Thread.sleep;
+//import com.sun.jna.Native;
 
+public class Main {
+    static String CsTitle = "Counter-Strike: Global Offensive - Direct3D 9";
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        Robot robot = new Robot();
     }
 }
