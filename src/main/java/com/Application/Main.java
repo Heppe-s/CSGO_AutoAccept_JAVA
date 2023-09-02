3cc1d2a0fdb2962b32a0be38190abd0ec3b1257c
Guilherme Felipe <glipecoelho2@gmail.com>
2023-09-02 04:25:15 -0300

    Third commit. I redid the JNA imports to correct an error and prepared the function to recognize when the CSGO window is open or not. Dependencies and .jar files have been installed into the program.

--- src/main/java/com/Application/Main.java
+++ src/main/java/com/Application/Main.java
@@ -1,13 +1,33 @@
 package com.Application;
 
-import java.awt.*;
-import java.awt.event.InputEvent;
-import static java.lang.Thread.sleep;
-//import com.sun.jna.Native;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.win32.StdCallLibrary;
+
 
 public class Main {
     static String CsTitle = "Counter-Strike: Global Offensive - Direct3D 9";
-    public static void main(String[] args) throws AWTException, InterruptedException {
-        Robot robot = new Robot();
+
+    public static void main(String[] args) {
+        boolean checker;
+
+        Pointer hWnd = User32.INSTANCE.FindWindowA(null, CsTitle);
+        if(hWnd == null){
+            System.out.println("A janela: "+CsTitle+" não foi encontrada.");
+        } else {
+            System.out.println("A janela: "+CsTitle+" foi encontrada, handle: "+hWnd);
+        }
+
+        checker = hWnd != null;
+        System.out.println(checker);
+        }
+        
+
+    public interface User32 extends StdCallLibrary{
+        User32 INSTANCE = Native.loadLibrary("user32", User32.class);
+
+        Pointer FindWindowA(String lpClass, String lpWindowName);
+
     }
+
 }
